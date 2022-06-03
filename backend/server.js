5d13bec777b0f293b6ba074fdded2387b6b62051
Nicholas Iyamabo <niyamabo@gmail.com>
2022-06-03 21:01:51 +0100

    extract frontend + delete backend

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = require('./config/keys');
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log("Connection success");
-    client.close();
-});
