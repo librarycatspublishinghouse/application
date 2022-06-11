2f39fa50d9dd2e3f5d0d82ab27b7551185b40dca
Nicholas Iyamabo <niyamabo@gmail.com>
2022-06-11 10:35:46 +0100

    card on hover using conditional styling

--- src/components/pages/home/Recomendations/Card/Card.js
+++ src/components/pages/home/Recomendations/Card/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState} from 'react';
 
 import style from './Card.module.css'
 import { Link } from 'react-router-dom';
@@ -10,17 +10,25 @@ import { Link } from 'react-router-dom';
 
 const Card = (props) => {
    
-    
+    const[hover, setHover] = useState(false)
    
+    const hoverChange = () => {
+        setHover(true);
+    }
+    const hoverChangeBack = () => {
+        setHover(false);
+    }
+
 
     return (
-        <div className={style.card} >
+        <div className={style.card} onMouseOver = {hoverChange} onMouseLeave = {hoverChangeBack}>
             <Link to = {"/publication/"+props.publicationID}  >
             <div className= {style.card_inner}>
                 <div className= {style.image_container}>
                     <img className = {style.image_item} src={`${process.env.REACT_APP_BACKEND_URL}`+props.imageLink} alt="book cover"/> 
                 </div>
-                <div className = {style.info_container}>
+                <div className = {style.info_container} style = {hover ? {height:'100px',
+    transition: 'ease-in-out 0.25s'}:{}}>
                     
                     <div className = {style.author_container}>
                         {props.authorname}
