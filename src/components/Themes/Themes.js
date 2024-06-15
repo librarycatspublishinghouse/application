636c6f5313922591ee6f5f9f403b43f1655230c1
Nicholas Iyamabo <niyamabo@gmail.com>
2024-06-15 17:39:46 +0100

    adding the new publication

--- src/components/Themes/Themes.js
+++ src/components/Themes/Themes.js
@@ -1,16 +1,19 @@
 const themes = [
     {
-        backgroundColour : "#ffd299",
-        textColour : "#dd6236",
-    },
-    {
         backgroundColour : "#7FB3D5",
         textColour : "#195775",
+    },
+
+
+    {
+        backgroundColour : "#ffd299",
+        textColour : "#dd6236",
     },
     {
         backgroundColour : "#9B111E",
         textColour : "#ffaea0",
     },
+
     {
         backgroundColour : "#61bc84",
         textColour : "#345e37",
--- src/components/pages/home/Promo/PromotionsConfig/PromotionsConfig.js
+++ src/components/pages/home/Promo/PromotionsConfig/PromotionsConfig.js
@@ -2,10 +2,16 @@
 
 
 export const PromotionsConfig = {
+    "Blue Moon": {
+        image: "/pictures/promotions/blue-moon-book.webp",
+        title: "Blue Moon",
+        description: "Blue Moon, a debut collection of poetry by Rachel Deléglise, delves deep into the heart and mind to explore the ever-pertinent themes of identity, change, loss, and love in all forms. Spanning time and geography, from London Bridge to the Caribbean, the poems return repeatedly to metaphors of nature, interwoven with human sentiments and experiences to emphasise how inseparable we are from the natural world around us. Masterfully conveying the nuances of the highs and lows of life, Deléglise exposes the universal nature of our seemingly lonely struggles with existential questions about our place in the world. In doing so, her words drive home the need to not only embrace, but also to share all the imperfect, contradictory elements of ourselves that ultimately come to define what it means to be human."
+    },
     "Contemporary Subjects": {
         image: "/pictures/promotions/contemporary-subjects-book.webp",
         title: "Contemporary Subjects",
         description: "Strange journeys to deliver undisclosed packages through 'non-descript names of far-off hamlets no one wants,' war diaries, oppressive loneliness and abhorrence for other people, or the depths of intergenerational trauma, all form part of what are indeed contemporary subjects. Take part in this collection of short stories and explore these themes through the prose of our newly published writers."
     },
+
 
 }
